70802865832f1ffbe6f38e2c3331e07b1cf59ee6
AllocotorXy <allocatorxy@foxmail.com>
2017-01-29 21:07:54 +0800

    topBar

---
+++ js/topBar.js
@@ -0,0 +1,35 @@
+function selectAddr() {
+    var oHeader = document.getElementsByTagName('header')[0];
+    var oAddr = oHeader.getElementsByTagName('h2')[0].getElementsByTagName('p')[0];
+    var oAddrList = oHeader.getElementsByClassName('addrSelect')[0];
+    var oAddrListAnchor = oAddrList.getElementsByTagName('a');
+    var timer = null;
+
+    oAddr.onmouseover = oAddrList.onmouseover = function() {
+    	clearTimeout(timer);
+        oAddr.className = 'active';
+        oAddrList.className = 'addrSelect show';
+    }
+    oAddr.onmouseout = oAddrList.onmouseout = function() {
+    	timer = setTimeout(function () {
+	        oAddr.className = '';
+	        oAddrList.className = 'addrSelect hidden';
+    	},100);
+    }
+    for (var i = 0; i < oAddrListAnchor.length; i++) {
+    	oAddrListAnchor[i].onclick = function () {
+    		for (var j = 0; j < oAddrListAnchor.length; j++) {
+    			oAddrListAnchor[j].className = '';
+    		}
+    		this.className = 'now';
+    		oAddr.innerHTML = this.innerHTML+'<i class="arrow-down"></i>';
+    		oAddr.className = '';
+    		oAddrList.className = 'addrSelect hidden';
+    	}
+    }
+}
+selectAddr();
+
+function topBarDropDown() {
+	
+}
